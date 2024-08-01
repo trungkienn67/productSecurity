66f7535d73f4db3f3b8d3b3fedfcafcde801c94a
Trung Kiên <153173408+trungkienn67@users.noreply.github.com>
2024-08-01 08:38:22 +0700

    Add files via upload

--- src/main/java/com/example/productsecurity/controller/ContentController.java
+++ src/main/java/com/example/productsecurity/controller/ContentController.java
@@ -3,10 +3,16 @@ package com.example.productsecurity.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
 @Controller
 public class ContentController {
     @GetMapping("/login")
     public String handleLogin() {
         return "maintenances/custom_login"; // Trả về tên view "login"
+    }
+    @GetMapping("/logout")
+    public String handleError() {
+        // Do something like logging
+        return "maintenances/custom_logout"; // Return a view named 'error'
     }
 }
